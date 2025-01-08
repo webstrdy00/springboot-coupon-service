1ae3d90e899ce9997115714de3a2f7e4579cf642
webstrdy00 <les_roka22@naver.com>
2025-01-08 10:24:29 +0900

    init commit

---
+++ src/main/java/com/hexagonal/coupon_service/CouponServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hexagonal.coupon_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponServiceApplication.class, args);
+    }
+
+}
